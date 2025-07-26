ae29c63a14f2244058bb456ec2590eb909a083a4
Turner Houghton <t.q.houghton@gmail.com>
2025-07-26 17:17:13 -0600

    added highlight.js client-side

--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -60,13 +60,12 @@ export const Layout = ({
             href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
             rel="stylesheet"
           />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/a11y-dark.min.css" />
           <link rel="stylesheet" href="/public/styles/tailwind.css" />
-          <script
-            src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js"
-            integrity="sha384-Akqfrbj/HpNVo8k11SXBb6TlBWmXXlYQrCSqEWmyKJe+hDm3Z/B2WVG4smwBkRVm"
-            crossorigin="anonymous"
-          />
-          <script src="https://unpkg.com/hyperscript.org@0.9.14"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/2.0.6/htmx.min.js" integrity="sha512-fzOjdYXF0WrjlPAGWmlpHv2PnJ1m7yP8QdWj1ORoM7Bc4xmKcDRBOXSOZ4Wedia0mjtGzXQX1f1Ah1HDHAWywg==" crossorigin="anonymous" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/hyperscript/0.9.14/_hyperscript.min.js" integrity="sha512-l43sZzpnAddmYhJyfPrgv46XhJvA95gsA28/+eW4XZLSekQ8wlP68i9f22KGkRjY0HNiZrLc5MXGo4z/tM2QNA==" crossorigin="anonymous" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js" integrity="sha512-EBLzUL8XLl+va/zAsmXwS7Z2B1F9HUHkZwyS/VKwh3S7T/U0nF4BaU29EP/ZSf6zgiIxYAnKLu6bJ8dqpmX5uw==" crossorigin="anonymous" />
+          <script>hljs.highlightAll();</script>
         </head>
         <body class="bg-black flex flex-col min-h-full grow items-center justify-center">
           <ErrorBoundary>
